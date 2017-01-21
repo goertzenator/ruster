b3522aa8d0954d8901c7e85515d1faa2f2e104b8
Daniel Goertzen <daniel.goertzen@gmail.com>
2017-01-21 13:34:22 -0600

    borrow test setup from erlang_nif-sys

--- tests/testapp/mynifmod.erl
+++ tests/testapp/mynifmod.erl
@@ -1,14 +1,38 @@
--module(test_nif).
+-module(mynifmod).
+
+-include_lib("eunit/include/eunit.hrl").
 
 -compile(export_all).
 
 -on_load(init/0).
 
--include_lib("eunit/include/eunit.hrl").
-
 init() ->
-	{ok, Lib} = find_crate:find_library(test_app, "test_nif", "test_nif"),
+    {ok, Lib} = find_library("mynifmod", "mynifmod"),
     ok = erlang:load_nif(Lib, 0).
+
+%% butchered version of https://github.com/goertzenator/find_crate/blob/master/src/find_crate.erl
+find_library(CrateName, LibName) ->
+	Wildcard = "priv/crates/" ++ CrateName ++ "/{lib,}" ++ LibName ++ ".{so,dll}",
+	case filelib:wildcard(Wildcard) of
+		[Lib] -> {ok, filename:rootname(Lib)};
+		[] -> {error, not_found};
+		_ -> {error, multiple_matches}
+	end.
+
+
+simple_test_() -> [
+		?_assertEqual(6, times2(3)),
+		?_assertEqual(self(), test_enif_make_pid())
+	].
+
+
+
+times2(_X)           -> exit(nif_library_not_loaded).
+test_enif_make_pid() -> exit(nif_library_not_loaded).
+
+
+
+
 
 dynamic_atom() -> exit(nif_library_not_loaded).
 dynamic_atom_test() -> ?assertEqual(an_atom, dynamic_atom()).
